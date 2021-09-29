3756d9cec35f28ba5542b835e5fd106fda2ee4c8
Rasel457 <rasel.aiub990a2gmail.com>
2021-09-29 14:37:17 +0600

    Portfolio using .net

---
+++ FirstApplication/FirstApplication/Controllers/PortfolioController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Bio()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+
+        public ActionResult TechnicalSkills()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
