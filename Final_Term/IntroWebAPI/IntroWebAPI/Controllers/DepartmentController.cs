87b2c47dbce8dec8ef96becc81d77fa24b971036
Rasel457 <rasel.aiub990a2gmail.com>
2021-11-16 19:37:16 +0600

    Introduction to Web API

---
+++ Final_Term/IntroWebAPI/IntroWebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,55 @@
+﻿using IntroWebAPI.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroWebAPI.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public List<DepartmentModel> Get()
+        {
+            //By default api maps methods with request verbs 
+            //UMS_AEntities db = new UMS_AEntities();
+            //return db.Departments.ToList();
+            List<DepartmentModel> depts = new List<DepartmentModel>();
+            for(int i = 0; i < 10; i++)
+            {
+                var d = new DepartmentModel()
+                {
+                    Id = i + 1,
+                    Name = "Department " + i
+                };
+                depts.Add(d);
+               // depts.Add(new DepartmentModel() { Id = i + 1, Name = "Department" + i });
+            }
+            return depts;
+            
+        }
+        public DepartmentModel Get(int id)
+        {
+            return new DepartmentModel() { Id = id, Name = "dep " + id };
+        }
+        public string Post(DepartmentModel d)
+        {
+            List<DepartmentModel> depts = new List<DepartmentModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                depts.Add(new DepartmentModel() { Id = i + 1, Name = "Department" + i });
+            }
+            depts.Add(d);
+            return "Added";
+        }
+        public string put(int id,DepartmentModel d)
+        {
+            return "Put";
+        }
+        public string Delete()
+        {
+            return "Delete";
+        }
+    }
+}
---
+++ Final_Term/IntroWebAPI/IntroWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,83 @@
+﻿using IntroWebAPI.Models;
+using IntroWebAPI.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using System.Web.Http.Cors;
+
+namespace IntroWebAPI.Controllers
+{
+    [EnableCors("*","*","*")]
+    public class StudentController : ApiController
+    {
+        [Route("api/Student/names")]
+        [HttpGet]
+        public List<string> StNames()
+        {
+            UMS_AEntities db = new UMS_AEntities();
+            var data = (from s in db.Students
+                        select s.Name).ToList();
+            return data;
+        }
+        public List<StudentModel> Get()
+        {
+            UMS_AEntities db = new UMS_AEntities();
+            var config = new MapperConfiguration(c =>
+                {
+                    c.CreateMap<Student, StudentModel>();
+                    c.CreateMap<Department, DepartmentModel>();
+                }
+            );
+            var mapper = new Mapper(config);
+            var data= mapper.Map<List<StudentModel>>(db.Students.ToList());
+            //var data = new List<StudentModel>();
+            //foreach (var s in db.Students)
+            //{
+            //    StudentModel st = new StudentModel()
+            //    {
+            //        Id = s.Id,
+            //        Name = s.Name,
+            //        Dob = s.Dob,
+            //        Gender = s.Gender,
+            //        Cgpa = s.Cgpa,
+            //        DepartmentId = s.DepartmentId
+            //    };
+            //    data.Add(st);
+
+            //}
+            return data;
+        }
+        public StudentModel Get(int id)
+        {
+            UMS_AEntities db = new UMS_AEntities();
+            var s = (from st in db.Students
+                       where st.Id == id
+                       select st).FirstOrDefault();
+
+            StudentModel sr = new StudentModel()
+            {
+                Id = s.Id,
+                Name = s.Name,
+                Dob = s.Dob,
+                Gender = s.Gender,
+                Cgpa = s.Cgpa,
+                DepartmentId = s.DepartmentId
+
+            };
+            return sr;
+
+        }
+        [Route("api/Student/Create")]
+        [HttpPost]
+        public void Post(Student s)
+        {
+            var db = new UMS_AEntities();
+            db.Students.Add(s);
+            db.SaveChanges();
+        }
+    }
+}
