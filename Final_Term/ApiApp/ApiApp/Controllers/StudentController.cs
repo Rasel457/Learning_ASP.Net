46427c25fec9ffa93b03c404015a3031eca530b5
Rasel457 <rasel.aiub990a2gmail.com>
2021-11-16 19:32:55 +0600

    Introduction to web API and Tair Architecture

---
+++ Final_Term/ApiApp/ApiApp/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL;
+using BLL.BEnt;
+
+namespace ApiApp.Controllers
+{
+    public class StudentController : ApiController
+    {
+        [Route("api/Student/Names")]
+        [HttpGet]
+        public List<string> GetNames()
+        {
+            return StudentService.GetNames();
+        }
+        [Route("api/Student/All")]
+        [HttpGet]
+        public List<StudentModel> GetAll()
+        {
+            return StudentService.Get();
+        }
+
+        [Route("api/Student/Create")]
+        [HttpPost]
+        public void Add(StudentModel s)
+        {
+            StudentService.Add(s);
+        }
+    }
+}
---
+++ Final_Term/ApiApp/DAL/StudentRepo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class StudentRepo
+    {
+        static UMS_AEntities db;
+        static StudentRepo()
+        {
+            db = new UMS_AEntities();
+        }
+        public static List<Student>Get()
+        {
+            return db.Students.ToList();
+        }
+        public static Student Get(int id)
+        {
+            return db.Students.FirstOrDefault(e => e.Id == id);
+        }
+        public static void Add(Student s)
+        {
+            db.Students.Add(s);
+            db.SaveChanges();
+        }
+        public static void Edit(Student s)
+        {
+            var ds= db.Students.FirstOrDefault(e => e.Id == s.Id);
+            db.Entry(ds).CurrentValues.SetValues(s);
+            db.SaveChanges();
+        }
+        public static void Delete(int id)
+        {
+            var st=db.Students.FirstOrDefault(e => e.Id == id);
+            db.Students.Remove(st);
+        }
+    }
+}
