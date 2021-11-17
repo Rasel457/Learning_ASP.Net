ee2ecbe533c17be18d757b002af128f0f951ec55
Rasel457 <rasel.aiub990a2gmail.com>
2021-11-17 20:00:07 +0600

    Lab Task

---
+++ Final_Term/NewsPortal/NewsPortal/Controllers/CatagoryController.cs
@@ -0,0 +1,41 @@
+﻿using BLL.BEnt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NewsPortal.Controllers
+{
+    public class CatagoryController : ApiController
+    {
+
+        [Route("api/Catagory")]
+        [HttpGet]
+        public List<CatagoryModel> GetAll()
+        {
+            return CatagoryService.Get();
+        }
+
+        [Route("api/Catagory/Create")]
+        [HttpPost]
+        public void Add(CatagoryModel n)
+        {
+            CatagoryService.Add(n);
+        }
+        [Route("api/Catagory/Edit/{id}")]
+        [HttpPost]
+        public void Edit(CatagoryModel n)
+        {
+            CatagoryService.Edit(n);
+        }
+        [Route("api/Catagory/Delete/{id}")]
+        public void Delete([FromUri] int id)
+        {
+            CatagoryService.Delete(id);
+            //return id;
+        }
+
+    }
+}
